2e3bc748a130b58972097ec5d92fb97792f6b3f0
MineWelt <58284244+MineWelt@users.noreply.github.com>
2021-07-10 18:58:19 +0800

    Update PCMBM.vhd

--- PCM/PCMBM.vhd
+++ PCM/PCMBM.vhd
@@ -1,45 +1,27 @@
-LIBRARY IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity pcm is
-	port(
-		clk : in std_logic after 10 us;
-		input : in std_logic_vector(12 downto 0);
-		output : out std_logic_vector(7 downto 0)
-	);
-end pcm;
-
-architecture code of pcm is
-	signal thirteen : std_logic_vector(12 downto 0);
-	signal eight  : std_logic_vector(7 downto 0);
-	
+library ieee;                 
+use ieee.std_logic_1164.all;     
+entity pcmbm is
+port(
+    clkin :in std_logic;               
+    B:in std_logic_vector(0 to 12);    
+    C:out std_logic_vector(0 to 7);   
+    clkout: out std_logic);            
+    end pcmbm;                         
+architecture behavior of pcmbm is
 begin
-	thirteen <= input;
-	process(clk,input)
-	begin
-	eight(7) <= thirteen(12);
-	if clk'event and clk='1' then
-		if thirteen(11) = '1' then
-			eight(6 downto 0) <= "000" & thirteen(10 downto 7);
-			elsif thirteen(10) = '1' then
-				eight(6 downto 0) <= "110" & thirteen(9 downto 6);
-				elsif thirteen(9) = '1' then
-					eight(6 downto 0) <= "101" & thirteen(8 downto 5);
-					elsif thirteen(8) = '1' then
-						eight(6 downto 0) <= "100" & thirteen(7 downto 4);
-						elsif thirteen(7) = '1' then
-							eight(6 downto 0) <= "011" & thirteen(6 downto 3);
-							elsif thirteen(6) = '1' then
-								eight(6 downto 0) <= "010" & thirteen(5 downto 2);
-								elsif thirteen(5) = '1' then
-									eight(6 downto 0) <= "001" & thirteen(4 downto 1);
-									else
-										eight(6 downto 0) <= "000" & thirteen(4 downto 1);
-		
-		
-		end if;
-		end if;
-		output <= eight;
-	end process;
-end architecture;
-		
+process(clkin,B)
+begin
+if clkin'event and clkin='1' then                    
+if B(1)='1' then C<=B(0)&"111"&B(2 to 5);   
+elsif  B(2)='1' then C<=B(0)&"110"&B(3 to 6);    
+    elsif  B(3)='1' then C<=B(0)&"101"&B(4 to 7);
+    elsif  B(4)='1' then C<=B(0)&"100"&B(5 to 8);
+    elsif  B(5)='1' then C<= B(0)&"011"&B(6 to 9);
+    elsif  B(6)='1' then C<= B(0)&"010"&B(7 to 10);
+    elsif  B(7)='1' then C<= B(0)&"001"&B(8 to 11);
+    else  C<=B(0)&"000"&B(8 to 11);
+    end if;
+end if;
+end process;
+clkout<=clkin;
+end  behavior;
